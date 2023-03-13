a7633e94ca5b034eea4e7b2e02578309c8399df1
Timur <bikb.timur@yandex.ru>
2023-03-13 23:36:43 +0500

    Initial Commit

---
+++ Rightandtarget_attempt2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rightandtarget_attempt2
+//
+//  Created by Аделия Исхакова on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
